ee8b180f4758b3876649d4c02928ecced0854fa8
Thinh <thinh@gmail.com>
2017-07-19 10:15:25 +0700

    First commit

---
+++ functions.php
@@ -0,0 +1,136 @@
+<?php 
+
+/**
+@khai bao hang gia tri
+	@ THEME_URL = lay duong dan thu muc theme 
+	@ CORE = lay duong dan cua thu muc /core
+**/
+define('THEME_URL', get_stylesheet_directory() );
+define('CORE', THEME_URL . "/core");
+
+/**
+ Nhung file /core/init.php
+**/
+require_once( CORE . "/init.php");
+
+
+/**
+ @ thiet lap chieu rong noi dung
+**/
+if ( !isset($content_width) ) {
+	$content_width = 660 ;
+}
+
+/**
+ @ khai bao chuc nang cua theme
+ */
+if (!function_exists('finazi_theme_setup')){
+	function finazi_theme_setup(){
+
+		/* thiet lap textdomain */
+		$language_folder = THEME_URL . '/languages';
+		load_theme_textdomain( 'finazi', $language_folder );
+
+		/* tu dong them link rss len <head>*/
+		add_theme_support('automatic-feed-links');
+
+		/* them post thumbnail*/
+		add_theme_support( 'post-thumbnails' );
+
+		/* post format */
+		add_theme_support( 'post-formats' , array(
+				'image',
+				'video',
+				'gallery',
+				'quote',
+				'link'
+			));
+
+		/* them title tag */
+		add_theme_support( 'title-tag');
+
+		/* them custom background */
+		$default_background = array(
+			'default-color' => '#e8e8e8'
+		);
+		add_theme_support('custom-background', $default_background);
+		add_theme_support( 'customize-selective-refresh-widgets' );
+		/* them menu */
+		register_nav_menus( array(
+			'primary-menu' => __( 'Primary Menu', 'finazi' ),
+		) );
+
+		/* Creat sidebar */
+		$sidebar = array(
+			'name' => __('Main Sidebar' , 'finazi'),
+			'id' => 'main-sidebar',
+			'description' => 'Main sidebar for Duc Thinh theme',
+			'class' => 'main-sidebar' ,
+			'before_title' => '<h3 class="widgettitle">',
+			'after_title' => '</h3>'
+		);
+		register_sidebar ($sidebar);
+	}	
+	add_action( 'init', 'finazi_theme_setup');
+}
+// add tag support to pages
+function tags_support_all() {
+	register_taxonomy_for_object_type('post_tag', 'page');
+}
+
+// ensure all tags are included in queries
+function tags_support_query($wp_query) {
+	if ($wp_query->get('tag')) $wp_query->set('post_type', 'any');
+}
+
+// tag hooks
+add_action('init', 'tags_support_all');
+add_action('pre_get_posts', 'tags_support_query');
+
+/*
+TEMPLATE FUNCTION
+ */
+
+
+
+
+
+
+
+/*===========nhúng file ==========*/
+function finazi_style(){
+	wp_register_style( 'main-style', get_template_directory_uri() . "/layout/css/style.css", 'all' );
+	wp_enqueue_style('main-style');
+
+	//font-awsome
+	wp_register_style( 'font-awsome-style', get_template_directory_uri() . "/layout/css/font-awsome.css", 'all' );
+	wp_enqueue_style('font-awsome-style');
+	wp_register_script('font-awsome-script', get_template_directory_uri() . "/layout/js/font-awsome.js" , array('jquery'));
+	wp_enqueue_script('font-awsome-script');
+
+	// bootstrap
+	wp_register_style( 'bootstrap-style', get_template_directory_uri() . "/layout/css/bootstrap.css", 'all' );
+	wp_enqueue_style('bootstrap-style');
+	wp_register_script('bootstrap-script', get_template_directory_uri() . "/layout/js/bootstrap.min.js" , array('jquery'));
+	wp_enqueue_script('bootstrap-script');
+	
+}
+add_action('wp_enqueue_scripts','finazi_style');
+
+
+function wpb_add_google_fonts() {
+
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Source+Sans+Pro', false );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+
+
+
+
+
+
+
+
+
